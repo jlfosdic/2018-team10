dc49591e6f3818ba197f7200aac669a796e87533
unknown <jlfosdic@mtu.edu>
2018-12-12 21:53:07 -0500

    Trailer ending

---
+++ DungeonManager/Assets/fade.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fade : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(FadeSprite.FadeImage(gameObject.GetComponent<SpriteRenderer>(), 0f, 0f, 0.2f));
+        StartCoroutine(Go());
+	}
+
+    public IEnumerator Go()
+    {
+        yield return new WaitForSeconds(2.2f);
+        StartCoroutine(FadeSprite.FadeImage(gameObject.GetComponent<SpriteRenderer>(), 0f, 1f, 1.5f));
+    }
+}
